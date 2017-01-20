d138cd70cc3d77d07f42cb696f1086b8b0975642
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-01-20 11:17:09 +0700

    fix: allow host

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(6$q72ng$ey1lh_vg(jnmk1@ujtjl%2*le8#0)5w+cqduzcc^*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘secret-hollows-64188.herokuapp.com’]
+ALLOWED_HOSTS = ['secret-hollows-64188.herokuapp.com']
 
 
 # Application definition
