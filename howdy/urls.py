4f8d428c417acc030294a39614238d5e47f04e48
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-01-20 14:56:44 +0700

    feat: porting from spring-hello to pyhton-hello

--- howdy/urls.py
+++ howdy/urls.py
@@ -4,5 +4,6 @@ from howdy import views
 
 urlpatterns = [
                url(r'^$', views.HomePageView.as_view()),
-               url(r'^callback$', views.callback, name='callback')
+               url(r'^callback$', views.callback, name='callback'),
+               url(r'^hello$', views.hello, name='hello')
                ]
--- howdy/views.py
+++ howdy/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from datetime import datetime
 
 
 # Create your views here.
@@ -13,10 +14,24 @@ def callback(request):
     
     verification_code = request.GET.get('verification_code')
     userid = request.GET.get('userid')
-    
-#    context = {
-#        'verification_code': verification_code,
-#        'userid': verification_url,
-#    }
 
     return HttpResponse ("Your verification code is: '{0}' and your user id is: '{1}'".format(verification_code, userid))
+
+
+def hello(request):
+
+    name = request.GET.get('name')
+
+    localHour = datetime.now().time().hour
+    if localHour >= 4 and localHour <= 10:
+        greeting = "Good morning, {0}!".format(name)
+    elif localHour >= 11 and localHour <= 15:
+        greeting = "Good day, {0}!".format(name)
+    elif localHour >= 16 and localHour <= 18:
+        greeting = "Good afternoon, {0}!".format(name)
+    elif localHour >= 19 and localHour <= 22:
+        greeting = "Good evening, {0}!".format(name)
+    else:
+        greeting = "Good night, {0}!".format(name)
+
+    return HttpResponse (greeting)
