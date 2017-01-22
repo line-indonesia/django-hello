0fa2f4dee7f11c4180ed9ba7a221d19007898278
Matthew Tanudjaja <mrexmelle@gmail.com>
2017-01-22 18:04:11 +0700

    fix: Fix wrong TIME_ZONE

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'django_hello.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -76,7 +76,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
